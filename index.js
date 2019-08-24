3f34756b2100bcd447796b7febb035024f5a01c6
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-08-24 04:34:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ function johnLennonFacts(facts) {
   return results;
 }
 
-function iLoveTheBeatles() {
-  var array [];
-  var i = 0;
+function iLoveTheBeatles(num) {
+  var array = [];
   do {
-    
-  }
+    array.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return array;
 }
