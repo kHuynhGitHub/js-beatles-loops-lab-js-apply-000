8333e20dfd6ed97091dadef746a6d14cab274ff2
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-08-24 04:21:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instuments) {
   var array = [];
-  for (i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return array;
